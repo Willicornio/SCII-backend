5d58e11c1956514171a61ea6e3406e2703c504a1
adrianavarrete <adri.navarrete.09@gmail.com>
2020-05-15 19:40:44 +0200

    Socket IO implementado, generadas las estructuras de comunicaciones

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
 var express = require('express');
 const morgan = require('morgan');
 const User = require('./model/userModel');
-const md5 =require('md5');
+const md5 = require('md5');
 const cors = require('cors');
 let db = null;
-const cors = require('cors');
+const SocketIO = require('socket.io');
+const HashMap = require('hashmap');
+
 
 const { mongoose } = require('./database');
 
 var app = express();
+var usuarios = new HashMap();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, function () {
+const server = app.listen(3000, function () {
     console.log("Servidor en marcha: ");
 });
 
+const io = SocketIO(server);
 
 app.post('/login', async (req, res) => {
 
@@ -25,9 +29,9 @@ app.post('/login', async (req, res) => {
 
     console.log("body.name :" + req.body.name);
 
-    User.findOne({"name": req.body.name, "pass": md5(req.body.pass)}).then((data) =>{
+    User.findOne({ "name": req.body.name, "pass": md5(req.body.pass) }).then((data) => {
         console.log("data : " + data);
-        res.status(200).json({data})
+        res.status(200).json({ data })
     }).catch((error) => {
         res.status(500).json(error);
     })
@@ -38,27 +42,27 @@ app.post('/login', async (req, res) => {
 
 app.get('/setDefaultState', async (req, res) => {
 
-    const alcalde = new User({id: "alcalde", name:"alcalde", pass: md5("alcalde")});
+    const alcalde = new User({ id: "alcalde", name: "alcalde", pass: md5("alcalde") });
     console.log(alcalde);
     await addUser(alcalde);
 
-    const concejal1 = new User({id: "concejal1", name:"concejal1", pass: md5("concejal1")});
+    const concejal1 = new User({ id: "concejal1", name: "concejal1", pass: md5("concejal1") });
     await addUser(concejal1);
 
-    const concejal2 = new User({id: "concejal2", name:"concejal2", pass: md5("concejal2")});
+    const concejal2 = new User({ id: "concejal2", name: "concejal2", pass: md5("concejal2") });
     await addUser(concejal2);
 
-    const concejal3 = new User({id: "concejal3", name:"concejal3", pass: md5("concejal3")});
+    const concejal3 = new User({ id: "concejal3", name: "concejal3", pass: md5("concejal3") });
     await addUser(concejal3);
 
-    const concejal4 = new User({id: "concejal4", name:"concejal4", pass: md5("concejal4")});
+    const concejal4 = new User({ id: "concejal4", name: "concejal4", pass: md5("concejal4") });
     await addUser(concejal4);
 
 
 });
 
 
- const addUser = async user => {
+const addUser = async user => {
 
     user.save().then((data) => {
         console.log("usuario correcto");
@@ -70,11 +74,97 @@ app.get('/setDefaultState', async (req, res) => {
 }
 
 
-app.get('/allUsers', async (req, res )=>{
-    User.find({}).then((data) =>{
+app.get('/allUsers', async (req, res) => {
+    User.find({}).then((data) => {
         res.status(200).json(data);
-    }).catch((error) =>{
+    }).catch((error) => {
         res.status(500).json(error);
+
+    });
+});
+
+io.on('connection', (socket) => {
+
+    var userSocket;
+
+
+    socket.on('usuario', (msg) => {
+        console.log('Se ha conectado ' + msg);
+
+        if (msg == "Alcalde") {
+
+            userSocket = msg;
+            usuarios.set(msg, socket.id);
+
+            socket.broadcast.emit('AlcaldeID', usuarios.get("Alcalde"));
+
+
+        } else {
+
+            userSocket = msg;
+            usuarios.set(msg, socket.id);
+
+
+            if (usuarios.get("Alcalde") != null) {
+                socket.emit('AlcaldeID', usuarios.get("Alcalde"));
+            }
+        }
+
+    });
+
+    socket.on('conectados', () => {
+
+        console.log("here")
+        var conectados = []
+        
+        usuarios.forEach((k,v) => {
+
+            conectados.push(v)
+
+        })
+
+        socket.emit('conectados', conectados);
+        socket.broadcast.emit('conectados',conectados)
+
+
+    });
+
+    socket.on('concejal-to-alcalde-type5', (mensaje) => {
+
+        console.log(mensaje)
+
+        //verificaciones correspondientes del proof
+
+        socket.broadcast.to(usuarios.get("alcalde")).emit('concejal-to-alcalde-type5', mensaje)
+
+        
+    });
+
+    socket.on('alcalde-to-concejal-type6', (mensaje) => {
+
+        console.log(mensaje)
+
+        //verificaciones correspondientes del proof si es correcto guardamos el mensaje para prueba de no repudio
+
+
+        
+    });
+
+
+    socket.on('disconnect', () => {
+
+        console.log("el usuario " + userSocket + " se ha desconectado ")
+        usuarios.delete(userSocket);
+
+        var conectados = []
+        
+        usuarios.forEach((k,v) => {
+
+            conectados.push(v)
+
+        })
+
+        socket.broadcast.emit('conectados',conectados)
 
     });
 });
